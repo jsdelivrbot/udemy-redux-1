5d4621cb580ed6cdd9ff3e657e61f82e44814663
Andy Tuttle <andytuttle12@gmail.com>
2017-01-16 09:08:00 -0500

    Started with es6 class components.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBZmfc1n_ygm1miyDAoYErriAV4Z_TLCec';
+
 // Create new component. Produce html.
 const App = () 	=>{
-	return <div>Sanity Check</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
+	
 } 
 // Add JSX to DOM.
 ReactDOM.render(
 	<App />,
 	document.querySelector('.container')
-)
+);
+
