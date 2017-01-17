02ca55455cfdab408fd070388df013b047f0e45f
Andy Tuttle <andytuttle12@gmail.com>
2017-01-17 08:40:09 -0500

    Added basic video thumbnails

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyBZmfc1n_ygm1miyDAoYErriAV4Z_TLCec';
 
-// Create new component. Produce html.
-const App = () 	=>{
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
-	
-} 
-// Add JSX to DOM.
+
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { videos: [] };
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+		});
+	}
+
+	render(){
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
+}
+
 ReactDOM.render(
 	<App />,
 	document.querySelector('.container')
